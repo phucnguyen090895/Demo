017ae7dade0c64e24c6b44904f3efcfca7743138
Phuc Nguyen <33108572+phucnguyen090895@users.noreply.github.com>
2025-05-08 15:24:52 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const products = [
+  { image:"1.png",id: 1, name: "Hazenut Cream", price: 50000, description: "Hazenut Cream croissant là sự kết hợp giữa lớp kem custard và phủ lên thêm một lớp hoa hồng khô" },
+  { image:"2.png",id: 2, name: "Matcha Cream", price: 60000, description: "Matcha Cream croissant là sự kết hợp giữa bánh croissant than tre và được phủ lên một lớp kem matcha" },
+  { image:"3.png",id: 3, name: "Strawberry Cream", price: 70000, description: "Strawberry Cream croissant là sự kết hợp giữa lớp nhân kem whipping dâu và phía trên là trái dâu tươi" },
+  { image:"4.png",id: 4, name: "Classic", price: 40000, description: "Classic croissant được làm từ bột mì, trứng, sữa hảo hạng và đặc biệt là bơ Pháp giúp cho bánh thơm ngon khó cưỡng" },
+  { image:"bestsales2.png",id: 5, name: "Almond Scream", price: 50000, description: "Almond Scream croissant được làm từ bột mì, trứng,</br> sữa hảo hạng và phía trên là hạnh nhân" },
+  { image:"bestsales4.png",id: 6, name: "Cheese & Meat Floss", price: 60000, description: "Cheese & Meat Floss croissant là sự kết hợp giữa lớp nhân cheese và thịt băm" },
+  { image:"bestsales3.png",id: 7, name: "Reaspberry Earl", price: 70000, description: "Reaspberry Earl croissant là sự kết hợp giữa lớp nhân kem whipping dâu</br> và phía trên là trái dâu tươi" }
+];
